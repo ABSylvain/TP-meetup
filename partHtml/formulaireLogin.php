bd5b017d3fd1d9813ce8ea215fd7e65df797029d
ABSylvain <arnaudbouriquesylvain@gmail.com>
2017-07-04 16:38:57 +0200

    commit

--- partHtml/formulaireLogin.php
+++
@@ -1,6 +0,0 @@
-
-<form method="POST" action="WorkPHP/login2.php">
-    <input type="text" name="login" placeholder="Pseudo"/>
-    <input type="password" name="mdp" placeholder="Mots de Passe"/>
-    <button name="submit">Envoyer</button>
-</form>
--- partHtml/logout.php
+++
@@ -1,6 +0,0 @@
-
-<form method="POST" action="./WorkPHP/logout2.php">
-    <button name="deco">Deconnextion</button>
-</form>
-
-
